df61fb4c36081f8942ae305cf506a9fb73f249e2
José de Ribamar Carvalho Martins Filho <ribaf@hotmail.com>
2020-09-28 16:32:51 -0300

    First commit

---
+++ src/main/java/br/com/smart/eletronicpoint/EletronicPointApplication.java
@@ -0,0 +1,13 @@
+package br.com.smart.eletronicpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicPointApplication.class, args);
+	}
+
+}
