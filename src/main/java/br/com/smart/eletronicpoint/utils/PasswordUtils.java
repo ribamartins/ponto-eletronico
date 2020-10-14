679d7b2c2a803b4463459eca18d6fdb1308b148e
= <=>
2020-10-14 09:45:28 -0300

    Created password utilities

--- src/main/java/br/com/smart/eletronicpoint/utils/PasswordUtils.java
+++ src/main/java/br/com/smart/eletronicpoint/utils/PasswordUtils.java
@@ -1,4 +1,22 @@
 package br.com.smart.eletronicpoint.utils;
 
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@NoArgsConstructor
 public class PasswordUtils {
+
+    private static final Logger log = LoggerFactory.getLogger((PasswordUtils.class));
+
+    public static String generateBCrypt(String password){
+        if(password == null){
+            return password;
+        }
+
+        log.info("Generating hash com o BCrypt");
+        BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+        return bCryptEncoder.encode(password);
+    }
 }
